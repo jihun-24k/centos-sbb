3d1901e7fa5bd084a2f3163039c58821f6d3b23b
jihun-24k <gi020477@gmail.com>
2022-09-16 09:33:20 +0900

    Chore: 세팅

---
+++ src/main/java/com/example/centossbb/CentosSbbApplication.java
@@ -0,0 +1,13 @@
+package com.example.centossbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentosSbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentosSbbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/centossbb/CentosSbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.centossbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentosSbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
